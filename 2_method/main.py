321e96de2778cf2a9dbb05177f45d0c775f6e91f
Kogoon <gojs712@gmail.com>
2021-10-11 06:59:53 +0000

    Routing according to the number of methods

--- 2_method/main.py
+++ 2_method/main.py
@@ -2,13 +2,13 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        return do_the_login()
-    else:
-        return show_the_login_form()
+@app.route('/login', methods=['GET'])
+def login_page():
+    return show_the_login_form()
 
+@app.route('/login', methods=['POST'])
+def login():
+    return do_the_login()
 
 def do_the_login():
     return 'Do the login'
