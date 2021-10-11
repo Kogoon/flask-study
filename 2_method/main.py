6b8ab5388077439e263c8dc329c931603a82d695
Kogoon <gojs712@gmail.com>
2021-10-11 06:56:33 +0000

    Branch according to the method

---
+++ 2_method/main.py
@@ -0,0 +1,19 @@
+from flask import Flask, request 
+
+app = Flask(__name__)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        return do_the_login()
+    else:
+        return show_the_login_form()
+
+
+def do_the_login():
+    return 'Do the login'
+
+def show_the_login_form():
+    return 'Show the login form'
+
+
